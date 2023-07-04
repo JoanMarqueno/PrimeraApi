e3a07ade918c736ca4fc570e0fc57aef63c1a49c
JoanMarqueno <joan.marquenyo@gmail.com>
2023-07-04 16:19:53 +0200

    cambios

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/saludoPost', methods=['POST'])
-def saludar():
+def saludar_post():
     nombre = request.json.get('nombre')
     if nombre:
         mensaje = f'Hola, {nombre}! ¡Bienvenido a la API!'
@@ -12,7 +12,7 @@ def saludar():
         return jsonify({'error': 'Nombre no proporcionado'}), 400
     
 @app.route('/saludoGet', methods=['GET'])
-def saludar():
+def saludar_get():
     return '¡Hola! Esta es mi API de saludo.'
 
 if __name__ == '__main__':
